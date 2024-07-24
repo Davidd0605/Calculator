39a2f8265e45fc85aedeb5c2043ead77fa58bd2c
David <dediudavidyt@gmail.com>
2024-07-24 11:25:59 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("myh1").textContent = "Hello";
+document.getElementById("myp").textContent = "I like your babies";
+
+let x;
+x = 18;
+window.alert("Hello Biden this is Zelensky");
