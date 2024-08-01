b48ef55cc81f00a0e71b2cc62d810191843181db
David <dediudavidyt@gmail.com>
2024-08-01 14:20:21 +0300

    counter

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-document.getElementById("myh1").textContent = "Hello";
-document.getElementById("myp").textContent = "I like your babies";
+let counter = 0;
 
-let x;
-x = 18;
-window.alert("Hello Biden this is Zelensky");
+document.getElementById("counter").textContent = counter;
+
+document.getElementById("inc").onclick = function() {
+    counter ++;
+    document.getElementById("counter").textContent = counter;
+}
+document.getElementById("dec").onclick = function() {
+    counter --;
+    document.getElementById("counter").textContent = counter;
+}
+document.getElementById("res").onclick = function() {
+    counter = 0;
+    document.getElementById("counter").textContent = counter;
+}
+
